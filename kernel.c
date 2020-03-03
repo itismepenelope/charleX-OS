132c6204f085efdf9f140cfa869752c59266776f
amzy-0 <azymiamin@gmail.com>
2020-03-04 01:41:11 +0330

    kernel updated !

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "keyboard.h"
 //index for video buffer array
 uint32 vga_index;
 //counter to store new lines
@@ -173,10 +174,13 @@ void kernel_entry()
   print_string("This code authored by amzy-0 (M.Amin Azimi.K)");
   print_new_line();
   print_new_line();
-  print_string("Retrieved from \"codeproject.com\"");
+  print_string("Reference :  \"codeproject.com\"");
   print_new_line();
   print_new_line();
   print_string("#end of kernel.c");
+  print_new_line();
+  print_new_line();
+  print_string("root@root #");
   print_new_line();
 
 }
